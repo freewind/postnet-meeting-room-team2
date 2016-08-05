51c48d633e201841cf290b8d0dd50835db576eef
AmberYLopez <1163281758@qq.com>
2016-08-05 09:20:12 +0800

    posnet-class

---
+++ src/core-zipcode-to-barcode.js
@@ -0,0 +1,13 @@
+let {getformatted,formattedGroup,buildAll,checkcode,printPostcode} = require('./core')
+let {loadAllBarcodes} = require('./barcodes');
+class printBarcode{
+    go(barcode){
+        let allBarcodes = loadAllBarcodes();
+        let formatted = getformatted(barcode);//格式化
+        let groupcodes = formattedGroup(formatted);//分组
+        let allcodes = buildAll(groupcodes, allBarcodes);//查找对应的数字
+        let checked = checkcode(allcodes);//验证检验码
+        return printPostcode(checked);
+    }
+}
+module.exports = printBarcode;
